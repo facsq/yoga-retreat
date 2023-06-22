c1bde3b658ae1140b7cd3f58cb95dbb29c002878
Jasraj S. Juneja <jasraj.209301235@muj.manipal.edu>
2023-06-23 03:56:24 +0530

    first commit
    Co-authored-by: aayush-dabral <aayush-dabral@users.noreply.github.com>

---
+++ src/components/LocationSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import tamiaImage from '../assets/tamia.png';
+import rishikeshImage from '../assets/rishikesh.png';
+
+const LocationSection = () => {
+  return (
+    <section className="py-12">
+      <h1></h1>
+    </section>
+  );
+};
+
+export default LocationSection;
